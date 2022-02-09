fc9ddd9afbd64fc5583bef90d3ccd72df824869f
OdysseyGuy <odysseyworldart@gmail.com>
2022-02-09 14:06:01 +0530

    Added extra symbols in Vulkan Loader. Updated config.yml

--- source/VulkanLoader.cpp
+++ source/VulkanLoader.cpp
@@ -9,6 +9,11 @@ int LoadVulkanLibrary() {
 
     vkCreateInstance = reinterpret_cast<PFN_vkCreateInstance>(dlsym(LibHandle, "vkCreateInstance"));
     vkDestroyInstance = reinterpret_cast<PFN_vkDestroyInstance>(dlsym(LibHandle, "vkDestroyInstance"));
+
+    vkCreateDevice = reinterpret_cast<PFN_vkCreateDevice>(dlsym(LibHandle, "vkCreateDevice"));
+    vkDestroyDevice = reinterpret_cast<PFN_vkDestroyDevice>(dlsym(LibHandle, "vkDestroyDevice"));
+
+    vkGetDeviceQueue = reinterpret_cast<PFN_vkGetDeviceQueue>(dlsym(LibHandle, "vkGetDeviceQueue"));
     
     vkEnumeratePhysicalDevices =
         reinterpret_cast<PFN_vkEnumeratePhysicalDevices>(dlsym(LibHandle, "vkEnumeratePhysicalDevices"));
@@ -21,26 +26,60 @@ int LoadVulkanLibrary() {
     vkCreateComputePipelines = reinterpret_cast<PFN_vkCreateComputePipelines>(dlsym(LibHandle, "vkCreateComputePipelines"));
     vkDestroyPipeline = reinterpret_cast<PFN_vkDestroyPipeline>(dlsym(LibHandle, "vkDestroyPipeline"));
 
+    vkGetBufferMemoryRequirements = 
+        reinterpret_cast<PFN_vkGetBufferMemoryRequirements>(dlsym(LibHandle, "vkGetBufferMemoryRequirements"));
+    vkGetImageMemoryRequirements =
+        reinterpret_cast<PFN_vkGetImageMemoryRequirements>(dlsym(LibHandle, "vkGetImageMemoryRequirements"));
+
     vkAllocateMemory = reinterpret_cast<PFN_vkAllocateMemory>(dlsym(LibHandle, "vkAllocateMemory"));
     vkFreeMemory = reinterpret_cast<PFN_vkFreeMemory>(dlsym(LibHandle, "vkFreeMemory"));
+
+    vkCreateCommandPool = reinterpret_cast<PFN_vkCreateCommandPool>(dlsym(LibHandle, "vkCreateCommandPool"));
+    vkDestroyCommandPool = reinterpret_cast<PFN_vkDestroyCommandPool>(dlsym(LibHandle, "vkDestroyCommandPool"));
+
+    vkAllocateCommandBuffers = reinterpret_cast<PFN_vkAllocateCommandBuffers>(dlsym(LibHandle, "vkAllocateCommandBuffers"));
+    vkFreeCommandBuffers = reinterpret_cast<PFN_vkFreeCommandBuffers>(dlsym(LibHandle, "vkFreeCommandBuffers"));
+    vkBeginCommandBuffer = reinterpret_cast<PFN_vkBeginCommandBuffer>(dlsym(LibHandle, "vkBeginCommandBuffer"));
+    vkEndCommandBuffer = reinterpret_cast<PFN_vkEndCommandBuffer>(dlsym(LibHandle, "vkEndCommandBuffer"));
+
+    vkCreateBuffer = reinterpret_cast<PFN_vkCreateBuffer>(dlsym(LibHandle, "vkCreateBuffer"));
+    vkCreateImage = reinterpret_cast<PFN_vkCreateImage>(dlsym(LibHandle, "vkCreateImage"));
 
     return 1;
 }
 
-// Instance
 PFN_vkCreateInstance vkCreateInstance;
 PFN_vkDestroyInstance vkDestroyInstance;
 
-// Physical Device
 PFN_vkEnumeratePhysicalDevices vkEnumeratePhysicalDevices;
 PFN_vkGetPhysicalDeviceProperties vkGetPhysicalDeviceProperties;
 PFN_vkGetPhysicalDeviceQueueFamilyProperties vkGetPhysicalDeviceQueueFamilyProperties;
 
-// Pipelines
+PFN_vkCreateDevice vkCreateDevice;
+PFN_vkDestroyDevice vkDestroyDevice;
+
+PFN_vkGetDeviceQueue vkGetDeviceQueue;
+
 PFN_vkCreateGraphicsPipelines vkCreateGraphicsPipelines;
 PFN_vkCreateComputePipelines vkCreateComputePipelines;
 PFN_vkDestroyPipeline vkDestroyPipeline;
 
-// Memory Allocations
+PFN_vkGetPhysicalDeviceMemoryProperties vkGetPhysicalDeviceMemoryProperties;
+
+PFN_vkGetBufferMemoryRequirements vkGetBufferMemoryRequirements;
+PFN_vkGetImageMemoryRequirements vkGetImageMemoryRequirements;
+
 PFN_vkAllocateMemory vkAllocateMemory;
 PFN_vkFreeMemory vkFreeMemory;
+
+PFN_vkCreateCommandPool vkCreateCommandPool;
+PFN_vkDestroyCommandPool vkDestroyCommandPool;
+
+PFN_vkAllocateCommandBuffers vkAllocateCommandBuffers;
+PFN_vkFreeCommandBuffers vkFreeCommandBuffers;
+PFN_vkBeginCommandBuffer vkBeginCommandBuffer;
+PFN_vkEndCommandBuffer vkEndCommandBuffer;
+
+PFN_vkCreateBuffer vkCreateBuffer;
+
+PFN_vkCreateImage vkCreateImage;
--- source/VulkanLoader.h
+++ source/VulkanLoader.h
@@ -14,11 +14,41 @@ extern PFN_vkEnumeratePhysicalDevices vkEnumeratePhysicalDevices;
 extern PFN_vkGetPhysicalDeviceProperties vkGetPhysicalDeviceProperties;
 extern PFN_vkGetPhysicalDeviceQueueFamilyProperties vkGetPhysicalDeviceQueueFamilyProperties;
 
+// Logical Device
+extern PFN_vkCreateDevice vkCreateDevice;
+extern PFN_vkDestroyDevice vkDestroyDevice;
+
+// Device Queue
+extern PFN_vkGetDeviceQueue vkGetDeviceQueue;
+
 // Pipelines
 extern PFN_vkCreateGraphicsPipelines vkCreateGraphicsPipelines;
 extern PFN_vkCreateComputePipelines vkCreateComputePipelines;
 extern PFN_vkDestroyPipeline vkDestroyPipeline;
 
+// Memory Properties
+extern PFN_vkGetPhysicalDeviceMemoryProperties vkGetPhysicalDeviceMemoryProperties;
+
+// Memory Requirements
+extern PFN_vkGetBufferMemoryRequirements vkGetBufferMemoryRequirements;
+extern PFN_vkGetImageMemoryRequirements vkGetImageMemoryRequirements;
+
 // Device Memory Allocations
 extern PFN_vkAllocateMemory vkAllocateMemory;
 extern PFN_vkFreeMemory vkFreeMemory;
+
+// Command Pool
+extern PFN_vkCreateCommandPool vkCreateCommandPool;
+extern PFN_vkDestroyCommandPool vkDestroyCommandPool;
+
+// Command Buffer
+extern PFN_vkAllocateCommandBuffers vkAllocateCommandBuffers;
+extern PFN_vkFreeCommandBuffers vkFreeCommandBuffers;
+extern PFN_vkBeginCommandBuffer vkBeginCommandBuffer;
+extern PFN_vkEndCommandBuffer vkEndCommandBuffer;
+
+// Buffers
+extern PFN_vkCreateBuffer vkCreateBuffer;
+
+// Images
+extern PFN_vkCreateImage vkCreateImage;
